154ba010dfbc3143d9114630283c5b0bf062e28a
Sudipta <hisudipta26@gmail.com>
2023-07-29 16:48:29 +0530

    feat: single video page

--- src/pages/SingleVideo/singleVideo.jsx
+++ src/pages/SingleVideo/singleVideo.jsx
@@ -7,10 +7,15 @@ import "./singleVideo.css";
 const SingleVideo = () => {
   const { ID } = useParams();
 
-  const { videoState } = useContext(VideoContext);
+  const { videoState, videoDispatch, isInWatchLater } =
+    useContext(VideoContext);
 
   const selectedVideo = videoState?.videoData?.find(
     ({ _id }) => _id === Number(ID)
+  );
+
+  const moreVideos = videoState?.videoData?.filter(
+    ({ _id }) => _id !== selectedVideo?._id
   );
 
   return (
@@ -24,14 +29,59 @@ const SingleVideo = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen=""
           ></iframe>
-          <div>
-            <img src="https://picsum.photos/40/40" alt="avatar" />
-            <div>
+          <div className="single-video-content-container">
+            <div className="single-video-content-left">
+              <img src="https://picsum.photos/40/40" alt="avatar" />
               <strong>{selectedVideo?.title}</strong>
             </div>
+            <div className="single-video-content-right">
+              {isInWatchLater(selectedVideo) ? (
+                <i
+                  className="fa-solid fa-clock"
+                  title="remove from watch later"
+                  onClick={() =>
+                    videoDispatch({
+                      type: "REMOVE_FROM_WATCH_LATER",
+                      payload: selectedVideo,
+                    })
+                  }
+                ></i>
+              ) : (
+                <i
+                  className="fa-regular fa-clock"
+                  title="add to watch later"
+                  onClick={() =>
+                    videoDispatch({
+                      type: "ADD_TO_WATCH_LATER",
+                      payload: selectedVideo,
+                    })
+                  }
+                ></i>
+              )}
+              <i
+                className="fa-solid fa-square-plus"
+                title="add to playlist"
+              ></i>
+              <i className="fa-solid fa-square-pen" title="add notes"></i>
+            </div>
+          </div>
+          <hr />
+          <div className="my-notes-container">
+            <h3>My Notes</h3>
           </div>
         </div>
-        <div className="more-videos-container"></div>
+        <div className="more-videos-container">
+          <h3>More Videos</h3>
+          {moreVideos?.map((video) => (
+            <div className="more-video-card">
+              <img src={video?.thumbnail} alt={video?.title} />
+              <div>
+                <strong>{video?.title}</strong>
+                <p>{video?.creator}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
